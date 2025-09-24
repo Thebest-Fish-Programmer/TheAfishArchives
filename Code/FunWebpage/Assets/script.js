1a105383bb10160043009c2e49414bd632d8f71d
Thebest-Fish-Programmer <engrdkg@gmail.com>
2025-09-24 17:22:31 -0600

    All The code

---
+++ Code/FunWebpage/Assets/script.js
@@ -0,0 +1,14 @@
+// Script
+function showTab(tabId) {
+    document.querySelectorAll('.tab-content').forEach(tab => {
+        tab.classList.remove('active');
+    });
+
+    document.getElementById(tabId).classList.add('active');
+}
+
+
+function loadTemplate(file) {
+    window.location.href = file;
+  }
+  
