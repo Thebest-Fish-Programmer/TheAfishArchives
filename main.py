42534c972a10b0a95c05fa647bf21a6a18100d7b
Thebest-Fish-Programmer <engrdkg@gmail.com>
2025-09-24 22:49:01 -0600

    Added 404 Handler and Fixed Links in AfishArchives.html

--- main.py
+++ main.py
@@ -1,26 +1,67 @@
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder="templates")
 
-# Main page
-@app.route('/')
-def index():
-    return render_template('AfishArchives.html')
+@app.route("/")
+def home_page():
+    return render_template("AfishArchives.html")
 
-# Serve PeopleArchives pages
-@app.route('/PeopleArchives/<path:filename>')
-def people_archives(filename):
-    return send_from_directory('templates/PeopleArchives', filename)
+# Adhvay
 
-# Serve other templates pages (static files directly in templates folder)
-@app.route('/<page>.html')
-def other_pages(page):
-    return send_from_directory('templates', f'{page}.html')
+@app.route("/Adhvay")
+def adhvay_page():
+    return render_template("PeopleArchives/Goonvay_Archive.html")
 
-# Serve assets
-@app.route('/Assets/<path:filename>')
+@app.route("/Adhvay/RelationshipStatus")
+def adhvay_relationship_page():
+    return render_template("Goonvay_relation.html")
+
+@app.route("/Adhvay/W_GoonvaySongs")
+def adhvay_songs_page():
+    return render_template("W_GoonvaySongs.html")
+
+
+# Priyam
+
+@app.route("/Priyam")
+def priyam_page():
+    return render_template("PeopleArchives/Priyam_Archive.html")
+
+@app.route("/Priyam/RelationshipStatus")
+def priyam_relationship_page():
+    return render_template("Priyam_relation.html")
+
+# Havish
+@app.route("/Havish")
+def havish_page():
+    return render_template("PeopleArchives/Havish_Archive.html")
+
+@app.route("/Havish/RelationshipStatus")
+def havish_relationship_page():
+    return render_template("Havish_relation.html")
+
+# Amir
+@app.route("/Amir")
+def amir_page():    
+    return render_template("PeopleArchives/Amir_Archive.html")
+
+@app.route("/Amir/RelationshipStatus")
+def amir_relationship_page():
+    return render_template("Amir_relation.html")
+
+
+# 404 Handler
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # "404.html" should live in your templates folder
+    return render_template("404.html"), 404
+
+# Assets
+
+@app.route("/Assets/<path:filename>")
 def assets(filename):
-    return send_from_directory('Assets', filename)
+    return send_from_directory("Assets", filename)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
